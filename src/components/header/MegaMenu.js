7c55581cef7345a547b5050492ae876e27282476
Mayank Pawar <pawarlucky51@gmail.com>
2025-07-11 21:08:11 +0530

    made MegaMenu as device compatible

--- src/components/header/MegaMenu.js
+++ src/components/header/MegaMenu.js
@@ -3,15 +3,15 @@ import React from "react";
 const MegaMenu = () => {
   return (
     <div
-      className="z-50 w-full bg-white px-48"
+      className="z-50 w-full bg-white px-4 sm:px-12 md:px-24 lg:px-48"
       role="region"
       aria-expanded="true"
       aria-hidden="false"
     >
       <div className="container mx-auto px-4 py-6">
-        <div className="flex flex-wrap -mx-4">
+        <div className="flex flex-col lg:flex-row flex-wrap -mx-4">
           {/* Claims Management Section */}
-          <div className="w-full md:w-1/3 px-4 mb-6 md:mb-0">
+          <div className="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
             <h3 className="text-xl font-bold text-black">Claims Management</h3>
             <div className="mt-2 border-t border-gray-300 w-full mb-4" />
             <SubMenu
@@ -34,9 +34,9 @@ const MegaMenu = () => {
           </div>
 
           {/* Risk Control Section */}
-          <div className="w-full md:w-1/3 px-4 mb-6 md:mb-0">
+          <div className="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
             <h3 className="text-xl font-bold text-black">Risk Control</h3>
-            <div className="mt-2 border-t border-gray-200 w-full  mb-4 " />
+            <div className="mt-2 border-t border-gray-200 w-full mb-4" />
             <SubMenu
               items={[
                 ["Claims Investigations", "claims-investigations/"],
@@ -49,8 +49,8 @@ const MegaMenu = () => {
           </div>
 
           {/* Technology Section */}
-          <div className="w-full md:w-1/3 px-4">
-            <h3 className="text-xl font-bold  text-black">Technology</h3>
+          <div className="w-full lg:w-1/3 px-4">
+            <h3 className="text-xl font-bold text-black">Technology</h3>
             <div className="mt-2 border-t border-gray-200 w-full mb-4" />
             <SubMenu
               items={[
@@ -70,14 +70,13 @@ const MegaMenu = () => {
 const SubMenu = ({ items, baseUrl }) => {
   return (
     <ul className="space-y-3">
-      {items.map(([title, path, subheading = false, isExternal = false,]) => (
+      {items.map(([title, path, subheading = false, isExternal = false]) => (
         <li key={title}>
           <a
-            className={`${subheading && "pl-8"} "text-[#003145] hover:underline items-center transition duration-200 block text-2lg"`}
+            className={`block text-[#003145] hover:underline transition duration-200 ${subheading ? "pl-4 sm:pl-6" : ""}`}
             href={isExternal ? path : `${baseUrl}${path}`}
             target={isExternal ? "_blank" : ""}
             rel={isExternal ? "noopener noreferrer" : ""}
-            // className="text-gray-700 hover:text-blue-800 transition duration-200 block"
           >
             {title}
           </a>
